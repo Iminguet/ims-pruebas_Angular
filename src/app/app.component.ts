8c1917ed225bff3da3c4575deacb59b1f8fb9168
Iminguet <i.minguet@hotmail.com>
2022-12-09 07:35:12 +0100

    iniciando proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ims-pruebas_Angular';
+  title = 'aprende Angular';
+
+  saludo = 'Hola Isra, estas aprendiendo Angular';
 }
