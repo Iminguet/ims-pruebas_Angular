04cb59c2497448a530583041a309b2ec08209c29
Iminguet <i.minguet@hotmail.com>
2022-12-11 08:04:12 +0100

    componentes inline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EmpleadosComponent, EmpleadoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  // templateUrl: './empleado.component.html',
+  //styleUrls: ['./empleado.component.scss']
+  template: '<p>Aquí iría un empleado</p>',
+  styles: ['p {background-color: red;padding: 0.5em;}'],
+})
+export class EmpleadoComponent {}
